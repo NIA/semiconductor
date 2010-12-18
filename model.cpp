356833c77605c45804957ce14b4875efbe4cf100
NIA <nia.fti@mail.ru>
2010-12-18 20:48:38 +0600

    Добавили константы и переменные для основных физических параметров задачи и функции для плотности носителей заряда.

--- model.cpp
+++ model.cpp
@@ -1,6 +1,14 @@
 #include "model.h"
 #include <cmath>
 
+namespace
+{
+    // CGS
+    const double m0 = 9.1093e-28; // gramm
+    const double k = 1.380662e-16; // erg * K
+    const double default_eff_density = 2.51e+19; // cm^-3
+}
+
 Model::Model()
 {
 }
@@ -17,4 +25,40 @@ void Model::fill_data()
         data.xs.push_back(x);
         data.ys.push_back(sin(x));
     }
+}
+
+double eff_density(double m, double T)
+{
+    return default_eff_density * pow((m/m0), (3./2.)) * pow((T/300), (3./2.));
+}
+
+void Model::compute_eff_dencities()
+{
+    eff_density_c = eff_density(mc, T);
+    eff_density_v = eff_density(mv, T);
+}
+
+double Model::fermi(double energy)
+{
+    return exp((fermi_level - energy)/(k * T));
+}
+
+double Model::density_n(double energy)
+{
+    return eff_density_c * fermi(energy);
+}
+
+double Model::density_p(double energy)
+{
+    return eff_density_v * (1 - fermi(energy));
+}
+
+double Model::density_donor_p(double energy)
+{
+    return density_donor * (1 - fermi(energy));
+}
+
+double Model::density_acceptor_n(double energy)
+{
+    return density_acceptor * fermi(energy);
 }
--- model.h
+++ model.h
@@ -18,11 +18,33 @@ class Model
 {
 private:
     DataSeries data;
+    double Eg; // erg
+    double Ed; // erg
+    double Ea; // erg
+    double density_donor; // cm^-3
+    double density_acceptor; // cm^-3
+    double T; // K
+    double mc; // gramm
+    double mv; // gramm
+
+    void compute_fermi_level();
+    double fermi_level; // erg
+
+    // fermi distribution
+    double fermi(double energy); // <no unit>
+
+    void compute_eff_dencities();
+    double eff_density_c; // cm^-3
+    double eff_density_v; // cm^-3
+
+    double density_n(double energy);
+    double density_p(double energy);
+
+    double density_donor_p(double energy);
+    double density_acceptor_n(double energy);
 
 public:
     Model();
-
     void fill_data();
-
     const DataSeries & get_data() const { return data; }
 };
